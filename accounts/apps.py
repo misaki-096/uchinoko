7c265855c97f612fbb43bf4ce5445c6f7ee59acd
magi-72 <misaki.a.0209@gmail.com>
2024-05-21 16:22:19 +0900

    環境構築　やり直し

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppFolderConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "app_folder"
+    name = "accounts"
